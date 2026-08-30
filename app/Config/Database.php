3776274ab15f802a7d7464bd9ee262ca0376d58d
Tobias <tobiascesarfacundoacevedo@gmail.com>
2026-08-30 15:17:51 -0300

    fix(database): agregar fallbacks getenv() para variables de entorno de conexion remota en Docker

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,6 +24,18 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->default['hostname'] = env('database.default.hostname', getenv('database.default.hostname') ?: (getenv('DATABASE_DEFAULT_HOSTNAME') ?: (getenv('MYSQLHOST') ?: $this->default['hostname'])));
+        $this->default['database'] = env('database.default.database', getenv('database.default.database') ?: (getenv('DATABASE_DEFAULT_DATABASE') ?: (getenv('MYSQLDATABASE') ?: $this->default['database'])));
+        $this->default['username'] = env('database.default.username', getenv('database.default.username') ?: (getenv('DATABASE_DEFAULT_USERNAME') ?: (getenv('MYSQLUSER') ?: $this->default['username'])));
+        $this->default['password'] = env('database.default.password', getenv('database.default.password') ?: (getenv('DATABASE_DEFAULT_PASSWORD') ?: (getenv('MYSQLPASSWORD') ?: $this->default['password'])));
+        $this->default['port']     = (int) env('database.default.port', getenv('database.default.port') ?: (getenv('DATABASE_DEFAULT_PORT') ?: (getenv('MYSQLPORT') ?: $this->default['port'])));
+        $this->default['DBDriver'] = env('database.default.DBDriver', getenv('database.default.DBDriver') ?: (getenv('DATABASE_DEFAULT_DBDRIVER') ?: $this->default['DBDriver']));
+    }
+
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
